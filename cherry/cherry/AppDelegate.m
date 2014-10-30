32b68fd402778b056642fb3387ba3d984b4bf992
Pavan Pethe <pavanp@cybage.com>
2014-10-30 12:42:49 +0530

    test 2

--- cherry/cherry/AppDelegate.m
+++ cherry/cherry/AppDelegate.m
@@ -17,6 +17,10 @@
 {
 }
 
+- (void) hello2
+{
+}
+
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
     return YES;

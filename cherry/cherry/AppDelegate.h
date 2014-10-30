7c6ada7051f6c555c48316006d45f1265c40adad
Pavan Pethe <pavanp@cybage.com>
2014-10-30 12:41:14 +0530

    test 1

---
+++ cherry/cherry/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  cherry
+//
+//  Created by Pavan Pethe on 10/30/14.
+//  Copyright (c) 2014 Rakuten INC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ cherry/cherry/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  cherry
+//
+//  Created by Pavan Pethe on 10/30/14.
+//  Copyright (c) 2014 Rakuten INC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
